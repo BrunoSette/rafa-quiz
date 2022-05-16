dc44dcfce8f00ab776a9df5c7fd6fe19ac15bb9b
Bruno <brunosette@gmail.com>
2022-05-16 16:13:01 -0400

    change new english words

--- util/config.js
+++ util/config.js
@@ -2,24 +2,24 @@
 //https://ttsmp3.com/text-to-speech/French/
 
 export const list = [
-  "leaf", //1
-  "leaves", //2
-  "wolf", //3
-  "wolves", //4
-  "potato", //5
-  "potatoes", //6
-  "roof", //7 
-  "roofs", //8
-  "family", //9
-  "families", //10
-  "library", //11
-  "libraries", //12
-  "journey", //13
-  "journeys", //14
-  "hero", //15
-  "heros", //16
-  "ditch", //17
-  "ditches", //18
+  "shoes", //1
+  "clues", //2
+  "wound", //3
+  "junior", //4
+  "truth", //5
+  "duty", //6
+  "news", //7
+  "through", //8
+  "few", //9
+  "who", //10
+  "schoolroom", //11
+  "whose", //12
+  "conclusion", //13
+  "june", //14
+  "shampoo", //15
+  "cruel", //16
+  "choose", //17
+  "ruin", //18
 ];
 
 export const frenchWeather = [
